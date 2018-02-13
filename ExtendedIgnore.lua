82df8f5e90d9bb319d794efdaf165294a01ad372
Ignifazius <stefan.hobohm@gmail.com>
2018-02-13 14:50:33 +0100

    adding groupscan and refined event tracking

--- ExtendedIgnore.lua
+++ ExtendedIgnore.lua
@@ -2,26 +2,31 @@ local frame = CreateFrame ("Button", "ExtendedIgnoreFrame", UIParent) -- create 
 frame:RegisterEvent("ADDON_LOADED")
 frame:RegisterEvent("PLAYER_LOGIN")
 frame:RegisterEvent("PLAYER_LOGOUT")
+frame:RegisterEvent("GROUP_ROSTER_UPDATE")
 frame:SetScript("OnEvent", function (self,event,arg1,...)
-	if (event == "ADDON_LOADED" and arg1 =="Blizzard_AchievementUI") then
-		load_ignore_table()
+    --print(event, arg1)
+	if (event == "ADDON_LOADED" and arg1 =="Blizzard_InspectUI") then -- TODO replace arg1 with something better
+        load_ignore_table()
+        hooksecurefunc("ToggleDropDownMenu", BanButtonSetup);
 	end
 	if event == "PLAYER_LOGIN" then
 		load_ignore_table()
-        hooksecurefunc("ToggleDropDownMenu", BanButtonSetup);
 	end
 	if event == "PLAYER_LOGOUT" then
 		save_ignore_table()
-	end
+    end
+    if event == "GROUP_ROSTER_UPDATE" then
+        scanGroupForBanned()
+    end
 end)
 
 
-local function myChatFilter(self, event, msg, author, ...)
+function myChatFilter(self, event, msg, author, ...)
     --print(event, msg, author)
-    if isPlayerInIgnoreTable(author) then
+    if local_ignore_table ~= nil and isPlayerInIgnoreTable(author) then
         print("Ignored player found: "..author.."("..msg..")")
         if event == "CHAT_MSG_WHISPER" then
-            autoreply(author)
+            --autoreply(author)
         end
         if event == "CHAT_MSG_WHISPER_INFORM" then
             print("autoreply sent to "..author)
@@ -35,7 +40,6 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_SAY", myChatFilter)
 ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", myChatFilter)
 ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER_INFORM", myChatFilter)
 ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", myChatFilter)
-
 
 
 SLASH_EXTENTEDIGNORE1 = "/ban"
@@ -53,7 +57,7 @@ function ban(msg)
         reason = ""
     end
     --print(msg)
-    print(player)
+    --print(player)
     --print(reason)
     local _, _, playername, playerserver = string.find(player, "(%w+)-(%w+)")
     --print(playername, playerserver)
@@ -66,16 +70,40 @@ function isPlayerLegit()
 end
 
 function autoreply(player)
-    msg = L["defaultIgnore"]
+    local msg = L["defaultIgnore"]
     SendChatMessage(msg, "WHISPER", nil, player)
 end
 
 function banPlayerPopup(player)
+    --print("ban from menu")
     addToIgnoreTable(player, "TODO")
 end
 
+function findPlayer(player)
+
+end
+
+function scanGroupForBanned()
+    print("Scanning group...")
+    local count = GetNumGroupMembers();
+    for raidIndex=1, count do
+        --name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(raidIndex);
+        local name, _, _, _, _, fileName  = GetRaidRosterInfo(raidIndex);
+        local color = RAID_CLASS_COLORS[fileName]
+        print("Scanning ", name, "(", count, "/", raidIndex, ")", color);
+        if isPlayerInIgnoreTable(name) then --http://wowprogramming.com/docs/api/UnitClass -> class color
+            print(name, local_ignore_table[name])
+            myPrint("Player "..name.." is on your banlist for: "..local_ignore_table[name])
+        end
+    end
+end
+
+function myPrint(msg)
+    DEFAULT_CHAT_FRAME:AddMessage(msg)
+end
+
 function addToIgnoreTable(player, reason)
-    print(player, isPlayerInIgnoreTable(player))
+    --print(player, isPlayerInIgnoreTable(player))
     local msg = ""
     if not isPlayerInIgnoreTable(player) then
         local_ignore_table[player] = reason
@@ -102,8 +130,10 @@ function isPlayerInIgnoreTable(playername)
 end
 
 function printIgnoreTable()
-    for player, reason in pairs(local_ignore_table) do
-        msg = player
+    local tmpTable = local_ignore_table
+    table.sort(tmpTable)
+    for player, reason in pairs(tmpTable) do
+        local msg = player
         if reason ~= "" then
             msg = msg.." ("..reason..")"
         end
@@ -137,12 +167,13 @@ table.insert(UnitPopupMenus["FRIEND"], #UnitPopupMenus["FRIEND"]-1, "BAN");
 table.insert(UnitPopupMenus["PLAYER"], #UnitPopupMenus["FRIEND"]-1, "BAN");
 function BanButtonSetup(level, value, dropDownFrame, anchorName, xOffset, yOffset, menuList, button, autoHideDelay) -- src: https://eu.battle.net/forums/en/wow/topic/6444795639 // https://worldofwarcraft.com/en-gb/character/eredar/prr
     if dropDownFrame and level then
-        local buttonPrefix = "DropDownList" .. level .. "Button";
+        buttonPrefix = "DropDownList" .. level .. "Button";
         local i = 2;
         while (1) do
             local button = _G[buttonPrefix..i];
             if not button then break end;
             if button:GetText() == UnitPopupButtons["BAN"].text then
+                print("banbtn")
                 button.func = function() banPlayerPopup(_G[buttonPrefix.."1"]:GetText()) end
                 break;
             end
@@ -158,6 +189,11 @@ local b = CreateFrame("Button", "MyButton", UIParent, "UIPanelButtonTemplate")
 	b:SetScript("OnClick", function()
 		printIgnoreTable()
 	end)
+    b:SetMovable(true)
+    b:EnableMouse(true)
+    b:RegisterForDrag("LeftButton")
+    b:SetScript("OnDragStart", frame.StartMoving)
+    b:SetScript("OnDragStop", frame.StopMovingOrSizing)
     
     
     
